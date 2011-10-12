a3d9bb09deed9f4d4ca8394dcefbb3e2c9209e8d
Martin Evans <martindevans@gmail.com>
2011-10-12 03:56:08 +0100

    Implemented > and < signs Added Spout API which is used for redstone
    Signed-off-by: Martin Evans <martindevans@gmail.com>

--- src/coulombCraft/Signs/SignFactory.java
+++ src/coulombCraft/Signs/SignFactory.java
@@ -3,7 +3,10 @@ package coulombCraft.Signs;
 import org.bukkit.block.Block;
 import org.bukkit.block.Sign;
 import org.bukkit.entity.Player;
+import org.getspout.spoutapi.SpoutManager;
+
 import me.martindevans.CoulombCraft.CoulombCraft;
+import me.martindevans.CoulombCraft.Utility;
 
 public class SignFactory
 {
@@ -32,23 +35,66 @@ public class SignFactory
 			s.update();
 		}
 	}
-	
-	private static boolean CreateDisplay(String[] input, final Block b, Player p, final CoulombCraft plugin)
+
+	private static IQueryable FindQueryable(CoulombCraft plugin, Player p, Block b, String variable)
 	{
-		final String variable = input[1];
-		final IQueryable queryable = plugin.getQueryProvider().GetAnyAdjacentQueryable(b.getLocation(), variable);
+		IQueryable queryable = plugin.getQueryProvider().GetAnyAdjacentQueryable(b.getLocation(), variable);
 		
 		if (queryable == null)
 		{
 			p.sendMessage("[Coulomb]No queryable blocks nearby");
-			return false;
+			return null;
 		}
 		
 		if (!queryable.CanAnswer(variable))
 		{
 			p.sendMessage("[Coulomb]Blocks do not understand \"" + variable + "\"");
-			return false;
+			return null;
 		}
+		
+		return queryable;
+	}
+	
+	private static IQueryable ParseAsConstantOrVariable(CoulombCraft plugin, Player p, Block b, final String input)
+	{
+		try
+		{
+			final double d = Double.parseDouble(input);
+			
+			//Return a queryable which always returns this constant value
+			return new IQueryable()
+			{
+
+				@Override
+				public String Query(String variable)
+				{
+					if (variable.equals(input))
+						return "" + d;
+					else
+						return null;
+				}
+
+				@Override
+				public boolean CanAnswer(String variable)
+				{
+					return variable.equals(input);
+				}
+			};
+		}
+		catch (NumberFormatException ex)
+		{
+			//return an actual queryable
+			return FindQueryable(plugin, p, b, input);
+		}
+	}
+	
+	private static boolean CreateDisplay(String[] input, final Block b, Player p, final CoulombCraft plugin)
+	{
+		final String variable = input[1];
+		final IQueryable queryable = FindQueryable(plugin, p, b, variable);
+		
+		if (queryable == null)
+			return false;
 		
 		SignUpdateTask task = new SignUpdateTask(plugin.getServer().getScheduler(), b, new Runnable()
 		{
@@ -67,13 +113,85 @@ public class SignFactory
 		return true;
 	}
 
-	private static boolean CreateLessThanOperator(String[] input, final Block b, Player p, final CoulombCraft plugin)
-	{
+	private static boolean CreateLessThanOperator(String[] input, final Block b, final Player p, final CoulombCraft plugin)
+	{		
+		final String name1 = input[1];
+		final IQueryable arg1 = ParseAsConstantOrVariable(plugin, p, b, name1);
 		
+		final String name2 = input[3];
+		final IQueryable arg2 = ParseAsConstantOrVariable(plugin, p, b, name2);
+		
+		final Block[] adjacent = Utility.AdjacentBlocksNoDiagonals(b);
+		
+		SignUpdateTask task = new SignUpdateTask(plugin.getServer().getScheduler(), b, new Runnable()
+		{
+			public void run()
+			{
+				Double value1 = Double.parseDouble(arg1.Query(name1));
+				Double value2 = Double.parseDouble(arg2.Query(name2));
+				
+				if (value1 < value2)
+				{
+					for (Block adj : adjacent)
+					{
+						SpoutManager.getMaterialManager().getSpoutBlock(adj).setBlockPowered(true);
+						adj.getState().update();
+					}
+				}
+				else
+				{
+					for (Block adj : adjacent)
+					{
+						SpoutManager.getMaterialManager().getSpoutBlock(adj).setBlockPowered(false);
+						adj.getState().update();
+					}
+				}
+			}
+		});
+		final int taskId = plugin.getServer().getScheduler().scheduleSyncRepeatingTask(plugin, task, 5, plugin.getConfiguration().getInt("Core.Sign Update Ticks", 20));
+		task.AssociateTaskId(taskId);
+
+		return true;
 	}
 	
 	private static boolean CreateGreaterThanOperator(String[] input, final Block b, Player p, final CoulombCraft plugin)
 	{
+		final String name1 = input[1];
+		final IQueryable arg1 = ParseAsConstantOrVariable(plugin, p, b, name1);
 		
+		final String name2 = input[3];
+		final IQueryable arg2 = ParseAsConstantOrVariable(plugin, p, b, name2);
+		
+		final Block[] adjacent = Utility.AdjacentBlocksNoDiagonals(b);
+		
+		SignUpdateTask task = new SignUpdateTask(plugin.getServer().getScheduler(), b, new Runnable()
+		{
+			public void run()
+			{
+				Double value1 = Double.parseDouble(arg1.Query(name1));
+				Double value2 = Double.parseDouble(arg2.Query(name2));
+				
+				if (value1 > value2)
+				{
+					for (Block adj : adjacent)
+					{
+						SpoutManager.getMaterialManager().getSpoutBlock(adj).setBlockPowered(true);
+						adj.getState().update();
+					}
+				}
+				else
+				{
+					for (Block adj : adjacent)
+					{
+						SpoutManager.getMaterialManager().getSpoutBlock(adj).setBlockPowered(false);
+						adj.getState().update();
+					}
+				}
+			}
+		});
+		final int taskId = plugin.getServer().getScheduler().scheduleSyncRepeatingTask(plugin, task, 5, plugin.getConfiguration().getInt("Core.Sign Update Ticks", 20));
+		task.AssociateTaskId(taskId);
+
+		return true;
 	}
 }
--- src/me/martindevans/CoulombCraft/Utility.java
+++ src/me/martindevans/CoulombCraft/Utility.java
@@ -1,6 +1,20 @@
 package me.martindevans.CoulombCraft;
 
+import org.bukkit.block.Block;
+import org.bukkit.block.BlockFace;
+
 public class Utility
 {
-
+	public static Block[] AdjacentBlocksNoDiagonals(Block b)
+	{
+		return new Block[]
+		{
+			b.getRelative(BlockFace.NORTH),
+			b.getRelative(BlockFace.EAST),
+			b.getRelative(BlockFace.SOUTH),
+			b.getRelative(BlockFace.WEST),
+			b.getRelative(BlockFace.UP),
+			b.getRelative(BlockFace.DOWN),
+		};
+	}
 }
